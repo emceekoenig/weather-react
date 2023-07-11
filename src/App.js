a91bd988175c24571912ae2f8c871dde65da53cd
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2023-07-11 18:22:53 -0500

    Updated city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>hello world</h1>
-        <Weather city="Tokyo" />
+        <Weather city="Austin" />
       </header>
     </div>
   );
